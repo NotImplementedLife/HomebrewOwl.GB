bd9fe47d8e5bfb73dbcaaf3e512ff74dce5f46bf
NotImplementedLife <not.implemented.life@gmail.com>
2021-05-05 20:51:37 +0300

    added oam

---
+++ common/inc/oam.asm
@@ -0,0 +1,29 @@
+; Thxx : https://gbdev.gg8.se/wiki/articles/OAM_DMA_tutorial
+
+SECTION "OAM DMA routine", ROM0
+CopyDMARoutine:
+  ld  hl, DMARoutine
+  ld  b, DMARoutineEnd - DMARoutine ; Number of bytes to copy
+  ld  c, LOW(hOAMDMA) ; Low byte of the destination address
+.copy
+  ld  a, [hli]
+  ldh [c], a
+  inc c
+  dec b
+  jr  nz, .copy
+  ret
+
+DMARoutine:
+  ldh [rDMA], a
+  
+  ld  a, 40
+.wait
+  dec a
+  jr  nz, .wait
+  ret
+DMARoutineEnd:
+
+SECTION "OAM DMA", HRAM
+
+hOAMDMA::
+  DS DMARoutineEnd - DMARoutine ; Reserve space to copy the routine to
